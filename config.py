fbcec98d9c745dcf6a0211adc246b81c3fb36d13
mike <mike@mikes-iMac.local>
2016-08-14 15:14:53 +0200

    added migrations

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = 'my precious'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    #print (SQLALCHEMY_DATABASE_URI)
 
 
 class DevelopmentConfig(BaseConfig):
