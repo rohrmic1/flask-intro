707ec478a3cf6972f0422bde07704c05cc5f67fc
mike <mike@mikes-iMac.local>
2016-08-21 22:06:04 +0200

    installed bs4, requests, added linklist, browse

--- app.py
+++ app.py
@@ -3,7 +3,25 @@ from flask import Flask, render_template, redirect, \
     url_for, request, session, flash
 from functools import wraps
 from flask_sqlalchemy import SQLAlchemy
-import sqlite3
+#import sqlite3
+import requests
+from bs4 import BeautifulSoup
+import re
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
+import json
+from json import loads
+
+import string
+
+import ast
+#ast.literal_eval("{'x':1, 'y':2}")
 
 # create the application object
 app = Flask(__name__)
@@ -14,6 +32,7 @@ app = Flask(__name__)
 
 import os
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # create the sqlalchemy object
 db = SQLAlchemy(app)
@@ -47,6 +66,83 @@ def home():
 def welcome():
     return render_template('welcome.html')  # render a template
 
+@app.route('/linklist', methods=['GET', 'POST', 'HEAD'])
+def linklist():
+    errors = []
+    results = {}
+    #http://www.indeed.ch/Stellen?q=sql&l=zurich&start=0
+
+    if request.method == "POST":
+        # get url that the user has entered
+
+        for j in range(0,21,10):
+
+            try:
+                url = 'http://www.indeed.ch/Stellen?q=' + request.form['url'] + '&l=zurich&start=' + str(j)
+                r = requests.get(url)
+                soup = BeautifulSoup(r.content)
+                script_soup = soup.find('script', text=re.compile('jobmap')).string # Which in this case is a dict within string so,
+                #jobmap_regexp = 'jobmap\[3\]= ({.*?});'
+
+                for i in range(0,10):
+                    regexp_str = 'jobmap\[' + str(i) + '\]= ({.*?});'
+                    codej = getJobmap(regexp_str,script_soup)
+
+            except ValueError as err:
+                errors.append(
+                    "Unable to get URL. Please make sure it's valid and try again."
+                )
+                print(err)
+    return render_template('linklist.html', errors=errors, results=results)
+
+def getParams(url):
+    params = url.split("&")[0]
+    params = params.split('=')
+    pairs = zip(params[0::1], params[1::1])
+    answer = dict((k,v) for k,v in pairs)
+    return answer
+
+def getJobmap(jobmap_regexp,script_soup):
+    jobmap = re.compile(jobmap_regexp, re.DOTALL)
+    matches = jobmap.search(script_soup)
+    code_raw = (matches.group(1))
+    codej = code_raw.replace("jk", "'jk'")
+    codej = codej.replace("efccid", "'efccid'")
+    codej = codej.replace("srcid", "'srcid'")
+    codej = codej.replace("cmpid", "'cxmpid'")
+    codej = codej.replace("cmpesc", "'cxpesc'")
+    codej = codej.replace("cmplnk", "'cxplnk'")
+    codej = codej.replace("num", "'num'")
+    codej = codej.replace("srcname", "'srcname'")
+    codej = codej.replace("cmp", "'comp'")
+    codej = codej.replace("locid", "'lxcid'")
+    codej = codej.replace("loc", "'loc'")
+    codej = codej.replace("country", "'country'")
+    codej = codej.replace("zip", "'zip'")
+    codej = codej.replace("city", "'city'")
+    codej = codej.replace("title", "'title'")
+    codej = codej.replace("rd", "'rd'")
+    codej = codej.replace("\\", '#')
+    codej = codej.replace("'", '"')
+    codej = codej.replace("\\", "")
+    codej = codej.replace("#", "\\")
+    try:
+        output_all = json.loads(codej)
+        required_fields = ['jk', 'title', 'comp', 'loc']
+        output = {key:value for key, value in output_all.items() if key in required_fields}
+        json_obj = json.dumps(output)
+        print('joblink: ', output['jk'], ', title: ', output['title'], ', company: ', output['comp'], ', location: ', output['loc'], )
+        jstr = json.loads(json_obj)
+        print(jstr)
+    except ValueError as err:
+        print(err)
+
+
+
+@app.route('/browse', methods=['GET', 'POST'])
+def browse():
+    return render_template('browse.html')
+
 
 # route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
@@ -72,8 +168,8 @@ def logout():
 
 
 # connect to database
-def connect_db():
-    return sqlite3.connect('posts.db')
+#def connect_db():
+#    return sqlite3.connect('posts.db')
 
 
 # start the server with the 'run()' method
--- db_create.py
+++ db_create.py
@@ -5,11 +5,13 @@ from models import BlogPost
 db.create_all()
 
 # insert data
-db.session.add(BlogPost("Good", "I\'m good."))
-db.session.add(BlogPost("Well", "I\'m well."))
-db.session.add(BlogPost("Excellent", "I\'m excellent."))
-db.session.add(BlogPost("Okay", "I\'m okay."))
-db.session.add(BlogPost("postgres", "local postgres instance"))
+# db.session.add(BlogPost("Good", "I\'m good."))
+# db.session.add(BlogPost("Well", "I\'m well."))
+# db.session.add(BlogPost("Excellent", "I\'m excellent."))
+# db.session.add(BlogPost("Okay", "I\'m okay."))
+# db.session.add(BlogPost("postgres", "local postgres instance"))
+
+db.session.add(BlogPost("TBD", "{'comp': 'BRINE', 'jk': '80804946cbe61431', 'title': 'Markit EDM/SQL Server Database Developer', 'loc': 'Zürich, ZH'}"))
 
 # commit the changes
 db.session.commit()
